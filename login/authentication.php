be11cee874e96d4a45f6237c4cf2e36d1de16c45
rajmehransit <94622026+rajmehransit@users.noreply.github.com>
2022-01-27 01:24:42 +0530

    Update authentication.php

--- login/authentication.php
+++ login/authentication.php
@@ -4,15 +4,16 @@
     $userpassword = $_POST['pass'];  
       
         //to prevent from mysqli injection  
-        $username = stripcslashes($user_name);  
-        $userpassword = stripcslashes($user_password);  
-        $username = mysqli_real_escape_string($con, $user_name);  
-        $userpassword = mysqli_real_escape_string($con, $user_password);  
+        $username = stripcslashes($username);  
+        $userpassword = stripcslashes($userpassword);  
+        $username = mysqli_real_escape_string($con, $username);  
+        $userpassword = mysqli_real_escape_string($con, $userpassword);  
       
         
-        $sql = "SELECT *FROM register_user WHERE user_name = $username AND user_password = $usepassword"; 
+        
+        $sql = "select *from register_user where user_name = '$username' and user_password = '$userpassword'"; 
         $result = mysqli_query($con, $sql);  
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $row = mysqli_fetch_array( $result, MYSQLI_ASSOC);  
         $count = mysqli_num_rows($result);  
           
         if($count == 1){  
