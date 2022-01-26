3c30f3b2839f9511d98b7e04d010b4101d1f4d6d
rajmehransit <94622026+rajmehransit@users.noreply.github.com>
2022-01-27 01:25:15 +0530

    Delete connection.php

--- login/connection.php
+++
@@ -1,7 +0,0 @@
-<?php      
-   
-   $con = new PDO('mysql:host=localhost;port=3307;dbname=db','root','');
-    if(mysqli_connect_errno()) {  
-        die("Failed to connect with MySQL: ". mysqli_connect_error());  
-    }  
-?>  
